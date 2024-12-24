eb195c5218ca4d2b097b1db829ce40affd41c75b
Erfangh-80 <erfangh4871gholami@gmail.com>
2024-12-25 00:36:44 +0330

    up 5 image to 80

--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -5,7 +5,7 @@ const upload = require('../middleware/multer'); // استفاده از middlewar
 const { createReport, getReportsByCategory, getReport } = require('../controllers/reportController');
 
 // ایجاد گزارش جدید با آپلود تصاویر
-router.post('/', upload.array('images', 5), createReport); // حداکثر 5 تصویر
+router.post('/', upload.array('images', 80), createReport); // حداکثر 80تصویر
 
 // دریافت گزارش‌های مربوط به یک دسته‌بندی خاص
 router.get('/category/:categoryId', getReportsByCategory);
